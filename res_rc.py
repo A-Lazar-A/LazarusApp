8a45a9be08ae7e9f2da8e9ab0453edcaa327c6db
Nikita Lazarev <fub.lazar@mail.ru>
2023-02-07 00:45:10 +0300

    added resources for ui

---
+++ res_rc.py
@@ -0,0 +1,101 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.4.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x00\xc2\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4px\x22 viewBox=\x220 \
+0 24 24\x22 width=\x22\
+24px\x22 fill=\x22#FFF\
+FFF\x22><path d=\x22M0\
+ 0h24v24H0V0z\x22 f\
+ill=\x22none\x22/><pat\
+h d=\x22M19 13h-6v6\
+h-2v-6H5v-2h6V5h\
+2v6h6v2z\x22/></svg\
+>\
+\x00\x00\x00\xfc\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4px\x22 viewBox=\x220 \
+0 24 24\x22 width=\x22\
+24px\x22 fill=\x22#FFF\
+FFF\x22><path d=\x22M0\
+ 0h24v24H0V0z\x22 f\
+ill=\x22none\x22/><pat\
+h d=\x22M16 9v10H8V\
+9h8m-1.5-6h-5l-1\
+ 1H5v2h14V4h-3.5\
+l-1-1zM18 7H6v12\
+c0 1.1.9 2 2 2h8\
+c1.1 0 2-.9 2-2V\
+7z\x22/></svg>\
+\x00\x00\x00\xd1\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4px\x22 viewBox=\x220 \
+0 24 24\x22 width=\x22\
+24px\x22 fill=\x22#FFF\
+FFF\x22><path d=\x22M0\
+ 0h24v24H0V0z\x22 f\
+ill=\x22none\x22/><pat\
+h d=\x22M9 16.2L4.8\
+ 12l-1.4 1.4L9 1\
+9 21 7l-1.4-1.4L\
+9 16.2z\x22/></svg>\
+\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x12\
+\x0dL\xa5'\
+\x00a\
+\x00d\x00d\x00_\x00w\x00h\x00i\x00t\x00e\x00_\x002\x004\x00d\x00p\x00.\x00s\x00v\
+\x00g\
+\x00\x15\
+\x0f\x8f\xb3\x07\
+\x00d\
+\x00e\x00l\x00e\x00t\x00e\x00_\x00w\x00h\x00i\x00t\x00e\x00_\x002\x004\x00d\x00p\
+\x00.\x00s\x00v\x00g\
+\x00\x13\
+\x094\xe6G\
+\x00d\
+\x00o\x00n\x00e\x00_\x00w\x00h\x00i\x00t\x00e\x00_\x002\x004\x00d\x00p\x00.\x00s\
+\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00j\x00\x00\x00\x00\x00\x01\x00\x00\x01\xc6\
+\x00\x00\x01\x86\x0c\xa1\x1a\x22\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x86\x0c\xa1}\xce\
+\x00\x00\x00:\x00\x00\x00\x00\x00\x01\x00\x00\x00\xc6\
+\x00\x00\x01\x86\x0c\xa1\x056\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
