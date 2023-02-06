22d0a0132459c8a6992e0e9b11b132e2d82028e9
Nikita Lazarev <fub.lazar@mail.ru>
2023-02-07 00:36:24 +0300

    db update

--- data_base/db.py
+++ data_base/db.py
@@ -27,10 +27,11 @@ class DataBase():
         buy_currency TEXT NOT NULL,
         sell_price DECIMAL(10,4) DEFAULT 0,
         sell_currency TEXT,
-        income_rub DECIMAL(10,2),
-        income_usd DECIMAL(10,2),
         buy_date DATE NOT NULL,
-        sell_date DATE
+        sell_date DATE,
+        income_rub DECIMAL(10,2),
+        income_usd DECIMAL(10,2)
+        
         )""")
         self.connection.commit()
 
@@ -48,8 +49,8 @@ class DataBase():
             case 'rub':
                 buy_price_rub = item['buy_price']
                 response = requests.get('https://api.binance.com/api/v3/ticker/price',
-                                        params={'symbol': 'RUBUSDT'}).json()
-                buy_price_usdt = (item['buy_price'] * Decimal(response['price'])).quantize(Decimal('1.00'))
+                                        params={'symbol': 'USDTRUB'}).json()
+                buy_price_usdt = (item['buy_price'] / Decimal(response['price'])).quantize(Decimal('1.00'))
             case 'ada':
 
                 response = requests.get('https://api.binance.com/api/v3/ticker/price',
@@ -76,8 +77,8 @@ class DataBase():
                 case 'rub':
                     sell_price_rub = item['sell_price']
                     response = requests.get('https://api.binance.com/api/v3/ticker/price',
-                                            params={'symbol': 'RUBUSDT'}).json()
-                    sell_price_usdt = (item['sell_price'] * Decimal(response['price'])).quantize(Decimal('1.00'))
+                                            params={'symbol': 'USDTRUB'}).json()
+                    sell_price_usdt = (item['sell_price'] / Decimal(response['price'])).quantize(Decimal('1.00'))
                 case 'ada':
 
                     response = requests.get('https://api.binance.com/api/v3/ticker/price',
@@ -100,16 +101,49 @@ class DataBase():
         item['sell_price'] = str(item['sell_price'])
         self.cursor.execute(
             """INSERT INTO items VALUES(:name, 
-            :buy_price,:buy_currency,:sell_price,:sell_currency,:income_rub,:income_usd,:buy_date,:sell_date)""",
+            :buy_price,:buy_currency,:sell_price,:sell_currency,:buy_date,:sell_date,:income_rub,:income_usd)""",
             item)
         self.connection.commit()
+
+    def get_number_of_columns(self) -> int:
+        return self.cursor.execute("""SELECT COUNT(*) FROM pragma_table_info('items')""").fetchone()[0]
 
     def get_item(self, id: int = None):
         if id is not None:
             current = self.cursor.execute("""SELECT * FROM items WHERE ROWID = (?)""", (id,)).fetchall()
         else:
             current = self.cursor.execute("""SELECT * FROM items""").fetchall()
-        return current
+        return current[::-1]
+
+    def beautify_items(self, items):
+        answer = []
+        for row in items:
+            item = [row[0]]
+            match row[2]:
+                case 'rub':
+                    item.append(str(row[1]) + ' ₽')
+                case 'usdt':
+                    item.append(str(row[1]) + ' $')
+                case 'ada':
+                    item.append(str(row[1]) + ' ₳')
+                case 'eth':
+                    item.append(str(row[1]) + ' Ξ')
+            match row[4]:
+                case 'rub':
+                    item.append(str(row[3]) + ' ₽')
+                case 'usdt':
+                    item.append(str(row[3]) + ' $')
+                case 'ada':
+                    item.append(str(row[3]) + ' ₳')
+                case 'eth':
+                    item.append(str(row[3]) + ' Ξ')
+            item.append(date.fromisoformat(row[5]).strftime('%d.%m.%Y'))
+            item.append(date.fromisoformat(row[6]).strftime('%d.%m.%Y'))
+            item.append(str(row[7]) + ' ₽')
+            item.append(str(row[8]) + ' $')
+
+            answer.append(item)
+        return answer
 
     def delete_item(self, id: int = None):
         if id is not None:
@@ -123,8 +157,16 @@ class DataBase():
         current = self.cursor.execute(f"""SELECT * FROM items WHERE ((buy_date 
         between (?) and (?)) and sell_date is null)
         or (sell_date is not null and sell_date between (?) 
-        and (?))""", (date.today() - timedelta(days=days), date.today(), date.today() - timedelta(days=days),date.today())).fetchall()
+        and (?))""", (date.today() - timedelta(days=days), date.today(), date.today() - timedelta(days=days),
+                      date.today())).fetchall()
         return current
+
+    def calc_income(self, items):
+        answ = [0, 0]
+        for item in items:
+            answ[0] += Decimal(item[7]).quantize(Decimal('1.00'))
+            answ[1] += Decimal(item[8]).quantize(Decimal('1.00'))
+        return answ
 
     def get_all(self):
         current = self.cursor.execute("""SELECT * FROM items""").fetchall()
