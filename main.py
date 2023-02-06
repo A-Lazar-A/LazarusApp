2e2ddbe88875c15b8d2995086b7aa2e8212ab6fc
Nikita Lazarev <fub.lazar@mail.ru>
2023-02-07 00:45:52 +0300

    start the app

--- main.py
+++ main.py
@@ -1,67 +1,19 @@
 from data_base.db import DataBase
 from datetime import date, timedelta
 from decimal import Decimal
+from ui.mainwindow import MainWindow
+from PySide6.QtWidgets import QApplication
+import sys
 
 
 def main():
     db = DataBase()
     db.create_table()
-    today = date.today()
-    # item = {
-    #     'name': 'TEXT NOT NULL',
-    #     'buy_price': Decimal('100.5'),
-    #     'buy_currency': 'usdt',
-    #     'sell_price': None,
-    #     'sell_currency': None,
-    #     'buy_date': today,
-    #     'sell_date': None,
-    #     'income_rub': None,
-    #     'income_usd': None,
-    #
-    # }
-    # db.add_item(item)
-    # print(db.get_item())
-    # item = {
-    #     'name': 'TEXT NOT NULL',
-    #     'buy_price': Decimal('100'),
-    #     'buy_currency': 'usdt',
-    #     'sell_price': Decimal('120'),
-    #     'sell_currency': 'usdt',
-    #     'buy_date': today,
-    #     'sell_date': today,
-    #     'income_rub': None,
-    #     'income_usd': None,
-    #
-    # }
-    # db.add_item(item)
-    # item = {
-    #     'name': 'TEXT NOT NULL',
-    #     'buy_price': Decimal('100'),
-    #     'buy_currency': 'usdt',
-    #     'sell_price': Decimal('120'),
-    #     'sell_currency': 'usdt',
-    #     'buy_date': today - timedelta(days=6),
-    #     'sell_date': today - timedelta(days=5),
-    #     'income_rub': None,
-    #     'income_usd': None,
-    #
-    # }
-    # db.add_item(item)
-    # item = {
-    #     'name': 'TEXT NOT NULL',
-    #     'buy_price': Decimal('100'),
-    #     'buy_currency': 'usdt',
-    #     'sell_price': Decimal('120'),
-    #     'sell_currency': 'usdt',
-    #     'buy_date': today - timedelta(days=36),
-    #     'sell_date': today - timedelta(days=25),
-    #     'income_rub': None,
-    #     'income_usd': None,
-    #
-    # }
-    # db.add_item(item)
-    print(db.get_item())
-    print(db.get_sales(7))
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+    # db.beautify_items(db.get_item())
 
 
 if __name__ == '__main__':
